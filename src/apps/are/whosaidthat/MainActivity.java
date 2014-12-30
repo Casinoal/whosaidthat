fcbff1605174230250f91d28d8f2a939b4640dd7
CasinoAl <alexwhyatt@gmail.com>
2014-12-30 16:00:18 +0200

    Testing a Branch
    I added zoom to a github

--- src/apps/are/whosaidthat/MainActivity.java
+++ src/apps/are/whosaidthat/MainActivity.java
@@ -12,7 +12,8 @@ import android.widget.Button;
 
 public class MainActivity extends Activity{
 
-		
+	//zoom
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
